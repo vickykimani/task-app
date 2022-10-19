46008ac92b3d22c5f7c1ca2adda6d3406da0c85f
vickykimani <victorianightingale70@gmail.com>
2022-10-19 19:05:10 +0300

    js updated

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
 window.addEventListener('load', () => {
     const form = document.querySelector("#new-task-form");
+    const input = document.querySelector('#new-task-input');
+    const list_el = document.querySelector('#tasks');
+
+    form.addEventListener('submit', (e) => {
+        e.preventDefault(); //prevents default behaviour which is reloading the page
+
+        const task = input.value;
+        const task_el = document.createElement('div');
+        task_el.classList.add('task');
+
+        const task_content_el = document.createElement('div'); //create task element div
+        task_content_el.classList.add('content');
+
+        task_el.appendChild(task_content_el);
+
+        const task_input_el = document.createElement('input');
+        task_input_el.classList.add('text');
+        task_input_el.type = 'text'; //define task_input_element type
+        task_input_el.value = task; //stores value in task element
+    })
 });
